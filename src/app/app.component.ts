74adf321547ce77da0067e77604f6b256011c7a9
sai-kiran14 <saikiranessamaraju@gmail.com>
2021-06-24 16:45:01 +0530

    first-commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'count-app';
+  title = 'Counter Application';
+
+  count:number = 0;
+  inc = ()=>{
+    if(this.count==10){
+      this.count=this.count
+    }else
+    this.count=this.count+1;
+  }
+  dec =()=>{
+    if(this.count==0){
+      this.count=0
+    }else
+    this.count=this.count-1;
+  }
+  res =()=>{
+    this.count=0
+  }
 }
